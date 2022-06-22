5beb33e1d930ae26607445fc3b08419a851255c8
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-22 00:11:18 -0700

    Continue check for url

--- src/main.c
+++ src/main.c
@@ -25,6 +25,26 @@ int has_git(char *path) {
     return access(git_path, F_OK) == 0;
 }
 
+int is_local(char *path) {
+    char git_path[60];
+    strcpy(git_path, path);
+    strcat(git_path, "/.git/config");
+
+    FILE *fp;
+    char buffer[255];
+    fp = fopen(git_path, "r");
+    if (fp == NULL) {
+        return 0;
+    }
+
+    while (fgets(buffer, 255, fp)) {
+        printf("%s\n", buffer);
+    }
+
+    fclose(fp);
+	return 0;
+}
+
 int main() {
     struct dirent *de;
 
@@ -38,6 +58,8 @@ int main() {
     while ((de = readdir(dr)) != NULL) {
         char *name = de->d_name;
         if (is_directory(name) && !dont_show(name)) {
+            is_local(name);
+
             if (has_git(name)) {
                 cprint("git", FG_GREEN);
             } else {
